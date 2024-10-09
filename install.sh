41c7aef1b342ee46bcec33b10af05d1dcb035644
CyuanT <tan.yuan.cy@gmail.com>
2024-10-09 20:57:23 +0800

    added docker image creation and pull from repo

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum install npm -y
-sudo yum install node -y
-sudo yum install git -y
+sudo su
+yum update -y
+yum install npm -y
+yum install node -y
+yum install git -y
+yum install docker -y
+systemctl start docker
+systemctl status docker
+git clone https://github.com/CyuanT/simple-node-app.git
+cd ~/simple-node-app
+docker build -t simple-node-app .
+docker run -dp 8080:8080 simple-node-app
